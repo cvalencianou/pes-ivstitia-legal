ba6dbd40d0ffa834457482e5ac7fada05df2353a
blooregardkazoo <cvalencianou@gmail.com>
2023-01-31 16:40:31 +0000

    Se modifica utilidad base datos

---
+++ utilidades/baseDeDatos.js
@@ -0,0 +1,17 @@
+const mysql2 = require('mysql2/promise')
+
+const executePreparedStatement = async (preparedStatement, values) => {
+
+    const connection = await mysql2.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+
+    })
+
+    const [result] = await connection.execute(preparedStatement, values)
+    return result
+}
+
+module.exports = executePreparedStatement
