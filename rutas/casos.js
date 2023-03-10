0477bd7985d1cdb3f172fc7b3042e37b83d4c07c
puqui29 <paulina.fallas@ulatina.net>
2023-03-10 23:59:57 +0000

    Se actualiza codigo front y back -end de casos.

--- rutas/casos.js
+++ rutas/casos.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const { verificarJWT } = require('../middleware/autenticacion')
-const { obtenerCasos, crearCaso, filtrarCasos } = require('../controladores/casos')
+const { obtenerCasos, crearCaso, filtrarCasos, actualizarCaso, eliminarCaso } = require('../controladores/casos')
 
 router.get('/', verificarJWT, obtenerCasos)
-router.post('/', verificarJWT, crearCaso)
 router.get('/filtro', verificarJWT, filtrarCasos)
+router.post('/', verificarJWT, crearCaso)
+router.put('/', verificarJWT, actualizarCaso)
+router.delete('/', verificarJWT, eliminarCaso)
 
 module.exports = router
