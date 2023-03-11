992703d39e672ee3204244fd9548a72fc4d1acff
puqui29 <paulina.fallas@ulatina.net>
2023-03-11 00:00:59 +0000

    Se actualiza codigo de modulo cliente.

--- modelos/Cliente.js
+++ modelos/Cliente.js
@@ -5,21 +5,27 @@ class Cliente {
     async obtenerClientes(usuarioId) {
         return await executePreparedStatement('CALL clientes_obtener(?)', [usuarioId])
     }
+    
     async filtrarClientes(usuarioId, datoCliente) {
         return await executePreparedStatement('CALL clientes_filtrar(?,?)', [usuarioId, datoCliente])
     }
+
     async crearCliente(usuarioId, nombre, cedula, correo, telefonoMovil, telefonoFisico, direccion, tipoCedula) {
         return await executePreparedStatement('CALL cliente_crear(?,?,?,?,?,?,?,?)', [nombre, cedula, correo, , telefonoMovil, telefonoFisico, direccion, usuarioId, tipoCedula])
     }
+
     async buscarPorCedula(usuarioId, cedula) {
         return await executePreparedStatement('CALL cliente_buscar_por_cedula(?,?)', [usuarioId, cedula])
     }
+
     async buscarPorId(usuarioId, clienteId) {
         return await executePreparedStatement('CALL cliente_buscar_por_id(?,?)', [clienteId, usuarioId])
     }
+
     async actualizarCliente(clienteId, usuarioId, nombre, cedula, correo, telefonoMovil, telefonoFisico, direccion, tipoCedula) {
         return await executePreparedStatement('CALL clientes_actualizar(?,?,?,?,?,?,?,?,?)', [clienteId, usuarioId, nombre, cedula, correo, telefonoMovil, telefonoFisico, direccion, tipoCedula])
     }
+
     async eliminarCliente(clienteId, usuarioId) {
         return await executePreparedStatement('CALL cliente_eliminar_por_id(?,?)', [clienteId, usuarioId])
     }
