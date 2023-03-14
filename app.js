04ff9085dacbfa09bde4bc89267f7987d4c1bc9f
cvalencianou <cvalencianou@gmail.com>
2023-03-14 16:22:32 +0000

    Se agregan modulos de seguridad express

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const app = express()
 require('express-async-errors')
+const rateLimit = require('express-rate-limit')
+const helmet = require("helmet")
 var cookieParser = require('cookie-parser')
 const manejoErrores = require('./middleware/manejoErrores')
 const usuarios = require('./rutas/usuarios')
@@ -9,15 +11,30 @@ const clientes = require('./rutas/clientes')
 const registros = require('./rutas/registros')
 const actos = require('./rutas/actos')
 const casos = require('./rutas/casos')
+const httpError = require('http-errors')
+const { StatusCodes } = require('http-status-codes')
 
-// Se hace parsing a objeto JSON de string
-app.use(express.json())
-//Se hace parsing a cookies firmadas
-app.use(cookieParser(String(process.env.COOKIE_SECRET)))
+// Middleware para asegurar headers http aplicación Express
+app.use(helmet())
 //Se comparten vistas con y sin extensión html
 app.use(express.static('./vistas', {
     extensions: ['html']
 }))
+//Middleware para limitar ataques DoS o ataques fuerza bruta
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    handler: (req, res, next, options) => {
+        throw new httpError(StatusCodes.TOO_MANY_REQUESTS, 'HA EXCEDIDO LAS SOLICITUDES. INTENTE DE NUEVO EN 15 MINUTOS')
+    }
+})
+app.use(limiter)
+// Se hace parsing a objeto JSON de string
+app.use(express.json())
+//Se hace parsing a cookies firmadas
+app.use(cookieParser(String(process.env.COOKIE_SECRET)))
 
 //Rutas a los controladores
 app.use('/api/v1/usuarios', usuarios)
@@ -28,7 +45,7 @@ app.use('/api/v1/casos', casos)
 
 //Ruta para cualquier otra no existente
 app.use((req, res, next) => {
-   res.status(404).sendFile('vistas/no-encontrado.html', { root: __dirname })
+    res.status(404).sendFile('vistas/no-encontrado.html', { root: __dirname })
 })
 
 //Middleware para el manejo de errores y respuestas
--- rutas/usuarios.js
+++ rutas/usuarios.js
@@ -1,3 +1,5 @@
+const httpError = require('http-errors')
+const { StatusCodes } = require('http-status-codes')
 const express = require('express')
 const router = express.Router()
 //Importa controladores para que sean manejados por endpoints
@@ -5,11 +7,22 @@ const { crearUsuario, iniciarSesion, cerrarSesion, obtenerUsuarios, actualizarUs
 //Importa middleware autenticación
 const { verificarJWT, verificarAdministrador } = require('../middleware/autenticacion')
 
+const rateLimit = require('express-rate-limit')
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
+    handler: (req, res, next, options) => {
+        throw new httpError(StatusCodes.TOO_MANY_REQUESTS, 'HA EXCEDIDO LAS SOLICITUDES. INTENTE DE NUEVO EN 15 MINUTOS')
+    }
+})
+
 //Rutas de actos hacia controladores, sew verifica rol de administrador para algunas y en todas se valida token JWT válido
-router.post('/auth', iniciarSesion)
+router.post('/auth', limiter, iniciarSesion)
 router.get('/auth', cerrarSesion)
-router.post('/auth/restablecer', restablecerContrasena)
-router.put('/auth', cambiarContrasena)
+router.post('/auth/restablecer', limiter, restablecerContrasena)
+router.put('/auth', limiter, cambiarContrasena)
 router.get('/', verificarJWT, verificarAdministrador, obtenerUsuarios)
 router.post('/', verificarJWT, verificarAdministrador, crearUsuario)
 router.put('/:id', verificarJWT, verificarAdministrador, actualizarUsuario)
