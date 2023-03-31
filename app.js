3de0a623f5b3ae9003d9083cc55dd21c3f3c2438
cvalencianou <cvalencianou@gmail.com>
2023-03-31 19:27:20 +0000

    Se corrigen limites de express rate limit

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(express.static('./vistas', {
 //Middleware para limitar ataques DoS o ataques fuerza bruta
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    max: 3600, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     handler: (req, res, next, options) => {
